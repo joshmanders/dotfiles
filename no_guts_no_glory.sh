3ef7a11bd7208552686b862ad4f3900cfcd28435
Josh Manders <josh@joshmanders.com>
2015-07-18 21:00:50 -0500

    initial commit

---
+++ no_guts_no_glory.sh
@@ -0,0 +1,170 @@
+function heading () {
+  printf "💯  \e[00;36m$1\e[00m\n"
+}
+
+function notify () {
+  printf "⚡️  \e[00;33m$1\e[00m\n"
+}
+
+function success () {
+  printf "👍🏻  \e[00;32m$1\e[00m $2\n"
+}
+
+function fail () {
+  printf "👎🏻  \e[00;31m$1\e[00m $2\n"
+}
+
+function footing () {
+  printf "🙏🏼  \e[00;31m$1\e[00m $2\n"
+}
+
+function linebreak () {
+  printf "\n"
+}
+
+function welcome () {
+  heading "Welcome to Setemupatron™ 9001®!"
+  linebreak
+  sleep 1
+}
+
+function can_i_has_sudo () {
+  if [ "$(sudo -n uptime 2>&1 | grep "load" | wc -l)" -lt 1 ]; then
+    notify "We need sudo to do a few things"
+    sleep 2
+    sudo -v
+    linebreak
+    # Update existing `sudo` time stamp until we finish
+    while true; do
+      sudo -n true
+      sleep 60
+      kill -0 "$$" || exit
+    done 2>/dev/null &
+  fi
+}
+
+function install_homebrew () {
+  notify "Installing Homebrew"
+  sleep 1
+  # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 1&2>/dev/null
+  success "Installed" "Homebrew"
+  sleep 1
+  linebreak
+}
+
+function brew_packages () {
+  notify "Installing Brew Packages"
+  sleep 1
+  cat $(pwd)/data/brew | while read package; do
+    success "Installed" $package
+    sleep 1
+    # brew install $package 1&2>/dev/null
+  done
+  linebreak
+}
+
+function cask_applications () {
+  notify "Installing Brew Cask Applications"
+  sleep 1
+  cat $(pwd)/data/cask | while read application; do
+    success "Installed" $application
+    sleep 1
+    # brew cask install $application 1&2>/dev/null
+  done
+  linebreak
+}
+
+function atom_packages () {
+  notify "Installing Atom Packages"
+  sleep 1
+  cat $(pwd)/data/atom | while read package; do
+    success "Installed" $package
+    sleep 1
+    # atom install $package 1&2>/dev/null
+  done
+  linebreak
+}
+
+function npm_packages () {
+  notify "Installing NPM Packages"
+  sleep 1
+  cat $(pwd)/data/npm | while read package; do
+    success "Installed" $package
+    sleep 1
+    # atom install $package 1&2>/dev/null
+  done
+  linebreak
+}
+
+function remove_file () {
+  if [ -a $1 ]; then
+    success "Removed" $1
+    # rm -f $1
+  else
+    fail "Ignored" $1
+  fi
+}
+
+function remove_files () {
+  notify "Removing all files"
+  sleep 1
+
+  remove_file $HOME/.gitconfig
+  sleep 1
+
+  remove_file $HOME/.gitignore
+  sleep 1
+
+  remove_file $HOME/.ssh/config
+  sleep 1
+
+  remove_file $HOME/.zshrc
+  sleep 1
+
+  remove_file $HOME/.atom/config.cson
+  sleep 1
+
+  remove_file $HOME/.atom/keymap.cson
+  sleep 1
+
+  remove_file $HOME/.atom/snippets.cson
+  linebreak
+  sleep 2
+}
+
+function symlink_file () {
+  if [ -a $1 ]; then
+    fail "Ignored" $1
+  else
+    success "Symlinked" ${1//$HOME\/./\~/}
+    # ln -s ${1//$HOME\//$(pwd)/} $1
+  fi
+}
+
+function symlink_files () {
+  notify "Symlinking all files"
+  sleep 1
+
+  symlink_file $HOME/.gitconfig
+  sleep 1
+
+  symlink_file $HOME/.gitignore
+  sleep 1
+
+  # if [ -d "$HOME/.ssh" ]; then
+  #   mkdir $HOME/.ssh
+  # fi
+  symlink_file $HOME/.ssh/config
+  sleep 1
+
+  symlink_file $HOME/.gitconfig
+  linebreak
+  sleep 2
+}
+
+function thank_youuuuu () {
+  sleep 3
+  linebreak
+  footing "Skynet installed successfully!"
+  sudo -k
+}
---
+++ zshrc
@@ -0,0 +1,91 @@
+# Where are my dotfiles?
+export DOTFILES="$HOME/.files"
+
+# I like to use Atom; for now.
+export EDITOR=atom
+
+# Central timezone, what's up?
+export TZ=America/Chicago
+
+# Custom PATH, nawm sayn.
+export PATH=`cat /etc/paths | tr "\\n" ":" | sed 's/:$//'`
+
+# Android Emulation.
+export ANDROID_HOME=/usr/local/opt/android-sdk
+
+# For historical purposes.
+HISTSIZE=10000
+SAVEHIST=8500
+
+# Is antigen installed?
+if [ ! -d "$HOME/antigen" ]; then
+  # Nope! Install it.
+  git clone https://github.com/zsh-users/antigen.git $HOME/.antigen
+fi
+
+# Now source it.
+source $HOME/.antigen/antigen.zsh
+
+# Oh My ZSH!
+COMPLETION_WAITING_DOTS="true"
+antigen use oh-my-zsh
+
+# Use my custom theme.
+antigen theme killswitch/zsh-theme
+
+# ZSH dokku helper.
+export DOKKU_HOST='104.131.164.219'
+antigen bundle killswitch/zsh-dokku
+
+# Lets load up some bundles.
+antigen bundle git
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-history-substring-search
+
+# bind UP and DOWN arrow keys
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+
+# Apply that shizzle!
+antigen apply
+
+# Not sure what this is, yet.
+setopt nocorrectall
+
+# Typing `edit .` is easier than anything else.
+alias edit="$EDITOR"
+
+# Because sometimes you gotta be harsh.
+alias fucking="sudo"
+
+# And sometimes you gotta be nice.
+alias please="sudo"
+
+# Edit my dotfiles!
+alias dotfiles="edit $DOTFILES"
+
+# Flush out the DNS cache.
+alias flushDNS="dscacheutil -flushcache"
+
+# Clean up pesky .DS_Store all over.
+alias cleanupDS="find . -type f -name '*.DS_Store' -ls -delete"
+
+# Clean up multiple launch services.
+alias cleanupLS="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
+
+# Get my IP Address
+function ip() {
+  echo "IP Address: $(curl -s ip.appspot.com)"
+}
+
+# Reload this file
+function reload() {
+  echo "Reloading .zshrc"
+  source $HOME/.zshrc
+}
+
+# All executable paths
+function paths() {
+  echo -e ${PATH//:/\\n}
+}
