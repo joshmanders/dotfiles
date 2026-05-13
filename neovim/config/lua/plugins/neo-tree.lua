90e05cf686b95baad4131431d0629a8fa6fa3665
Josh Manders <josh@joshmanders.com>
2026-05-12 17:28:05 -0700

    refactor: tune neo-tree display and mappings
    Co-Authored-By: Dunnbot <dunnbot@joshmanders.com>

--- neovim/config/lua/plugins/neo-tree.lua
+++ neovim/config/lua/plugins/neo-tree.lua
@@ -8,7 +8,22 @@ return {
   },
   lazy = false,
   opts = {
+    source_selector = {
+      truncation_character = "",
+    },
+    default_component_configs = {
+      name = {
+        root_folder_formatter = function(path)
+          local parts = vim.split(path, "/")
+          if #parts >= 2 then
+            return parts[#parts - 1] .. "/" .. parts[#parts]
+          end
+          return parts[#parts]
+        end,
+      },
+    },
     filesystem = {
+      use_libuv_file_watcher = true,
       follow_current_file = { enabled = true },
       filtered_items = {
         hide_dotfiles = false,
@@ -17,7 +32,10 @@ return {
       },
     },
     window = {
-      width = 35,
+      width = 30,
+      mappings = {
+        ["<leader>n"] = "add",
+      },
     },
   },
   init = function()
