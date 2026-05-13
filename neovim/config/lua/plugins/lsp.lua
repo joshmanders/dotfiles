03bbee07d56ae6aa397c72cd2e26634332db7b54
Josh Manders <josh@joshmanders.com>
2026-05-12 17:28:02 -0700

    refactor: switch blink.cmp to super-tab preset
    Co-Authored-By: Dunnbot <dunnbot@joshmanders.com>

--- neovim/config/lua/plugins/lsp.lua
+++ neovim/config/lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
     version = "1.*",
     dependencies = { "rafamadriz/friendly-snippets" },
     opts = {
-      keymap = { preset = "default" },
+      keymap = { preset = "super-tab" },
       appearance = { nerd_font_variant = "mono" },
       completion = { documentation = { auto_show = true } },
       sources = {
