0bd95debd1d8c11b6f8d452546de6fef8048de94
Josh Manders <josh@joshmanders.com>
2026-05-12 17:27:46 -0700

    refactor: change neovim leader key to comma
    Co-Authored-By: Dunnbot <dunnbot@joshmanders.com>

--- neovim/config/init.lua
+++ neovim/config/init.lua
@@ -1,6 +1,6 @@
 -- Set leader key before anything else
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 -- Core configuration
 require("options")
